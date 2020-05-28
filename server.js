2c8c2e81377af5ae238c8497b02d06a6ed98ea29
Bryant Vail <BryantVail@gmail.com>
2020-05-27 22:51:19 -0400

    init

---
+++ server.js
@@ -0,0 +1,15 @@
+//server.js
+const express = require("express");
+const app = express();
+
+app.use(express.static('public'));
+
+//no admin rights needed for port 3000
+const runningPort = 3000;
+app.listen(runningPort, function () {
+  console.log(`app started on port ${runningPort}`);
+});
+
+
+
+
