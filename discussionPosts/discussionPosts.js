1d32e409dea883f64aa7e1b675d26a86c6b706a4
Bryant Vail <BryantVail@gmail.com>
2020-08-03 22:49:14 -0400

    Create discussionPosts.js

---
+++ discussionPosts/discussionPosts.js
@@ -0,0 +1,20 @@
+
+
+// Looking at these three sites as analyzed by the tool its fascinating to see how many redirects these sites make to complete the transaction- I'm usually upset about my browser stalling to work through all of them; I'm glad this is a prevalent metric today. I also found that other optimizations like caching and 'scaling' images are not executed well here, I would think that's out of convenience. I will say, with these two findings I get curious about how responsive and efficient I will be able to be for the web server when I  get more experience- some of these items seem like they can just be a matter of discipline.
+
+
+// The stat of 'serv[ing] resources from a consistent URL' really surprised me, and it makes me want to look further into these metrics and find out why they're so important to execute! That, and all of the metrics for styles and size efficiency like 'enable compression'- really cool.
+
+
+
+// Module 12 Discussion - Web site performance
+// 11 11 unread replies. 11 11 replies.
+
+// Open the following web page: https://gtmetrix.com/ (Links to an external site.). Use it to analyze nike.com, disney.com and apple.com.
+
+// After running the analysis, write down your thoughts on the following questions:
+
+//     Which website optimization task did the websites generally perform the worst on?
+//     Did you find any of the optimization tasks surprising or interesting?
+
+// Make sure your answer is concise (no longer than half a page).
